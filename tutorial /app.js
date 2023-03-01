b4990dc92d901727137195980ac0d62dadaf1cbc
mahdiar_gp <92169717+Mahdiargp@users.noreply.github.com>
2023-03-01 15:21:59 +0330

    Update app.js

--- tutorial /app.js
+++ tutorial /app.js
@@ -1,6 +1,11 @@
 function math(x,y) {
     console.log(x * y);
-}
+    
+    if(x > y) {
+        console.log("x bigger than y"); 
+        } else {
+            console.log("y bigger than x");
+        }
+    }
 math(15, 43);
 
-//befarmaeid (:
